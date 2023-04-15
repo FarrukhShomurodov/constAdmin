aa4fc042aede36843c7a30b40273fa3625fe19b9
farukh shomurodov <you@example.com>
2023-04-16 00:05:53 +0500

    first commit

---
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApplicationRequest;
+use App\Http\Resources\ApplicationResource;
+use App\Models\Application;
+use App\Services\ApplicationService;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class ApplicationController extends Controller
+{
+    private ApplicationService $applicationService;
+
+    public function __construct(ApplicationService $applicationService)
+    {
+        $this->applicationService = $applicationService;
+    }
+
+    /**
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $app = $this->applicationService->index();
+        return ApplicationResource::collection($app);
+    }
+
+    /**
+     * @param ApplicationRequest $request
+     * @return ApplicationResource
+     */
+    public function store(ApplicationRequest $request): ApplicationResource
+    {
+        $this->applicationService->store($request->validated());
+        return ApplicationResource::make($request);
+    }
+
+    /**
+     * @param Application $application
+     * @return ApplicationResource
+     */
+    public function app_done(Application $application): ApplicationResource
+    {
+        $app = $this->applicationService->app_done($application);
+        return ApplicationResource::make($app);
+    }
+}
---
+++ app/Services/ApplicationService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Application;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationService
+{
+    /**
+     * @return Collection|array
+     */
+    public function index(): Collection|array
+    {
+        return Application::query()->get();
+    }
+
+    /**
+     * @param $validated
+     * @return Model|Builder
+     */
+    public function store($validated): Model|Builder
+    {
+        $app = Application::query()->create($validated);
+        return $app->refresh();
+    }
+
+    /**
+     * @param Application $application
+     * @return Application|null
+     */
+    public function app_done(Application $application): ?Application
+    {
+        $application->update([
+            "state" => 1
+        ]);
+        return $application->fresh();
+    }
+}
